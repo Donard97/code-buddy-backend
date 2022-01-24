4d1ae243fd480c6fa1b753f669a0108baca81b60
Donard97 <donardgolaj@gmail.com>
2022-01-24 14:15:30 +0100

    Fix database and migrate

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ gem 'bootsnap', require: false
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
+group :production do
+  gem 'pg', '~> 1.1'
+end
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_24_112417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "meeting_types", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.string "image"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "color"
+    t.string "image"
+    t.string "link"
+    t.string "attendee_limit"
+    t.bigint "user_id", null: false
+    t.bigint "meeting_type_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["meeting_type_id"], name: "index_meetings_on_meeting_type_id"
+    t.index ["user_id"], name: "index_meetings_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "meeting_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["meeting_id"], name: "index_reservations_on_meeting_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "user_details", force: :cascade do |t|
+    t.string "full_name"
+    t.string "avatar"
+    t.date "birthday"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_details_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "meetings", "meeting_types"
+  add_foreign_key "meetings", "users"
+  add_foreign_key "reservations", "meetings"
+  add_foreign_key "reservations", "users"
+  add_foreign_key "user_details", "users"
 end

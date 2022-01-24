1305af9c47f493effc13e46fb805a1d026e058b9
ahzia <ah.ziayosfi@gmail.com>
2022-01-24 16:50:07 +0430

    fix linters issue

--- test/controllers/meeting_types_controller_test.rb
+++ test/controllers/meeting_types_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class MeetingTypesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
--- test/models/meeting_type_test.rb
+++ test/models/meeting_type_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class MeetingTypeTest < ActiveSupport::TestCase
   # test "the truth" do

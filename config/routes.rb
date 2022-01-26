901aef41527e7a820c442f810e7a51345e1724fd
Donard97 <donardgolaj@gmail.com>
2022-01-26 21:27:34 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  resources :reservations
-  resources :meetings
-  resources :meeting_types
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "users#index"
+  resources :meetings do 
+    get 'reservations', to: 'meetings#reservations'
+  end
 
   resources :users do
+    get 'meetings', to: 'user_details#meetings'
     resources :user_details
+    resources :reservations
+  end
+
+  resources :meeting_types do
+    get 'meetings', to: 'meeting_types#meetings'
   end
 end

a68c6927d1cb234a03ae5a84f9a9f4156ab23108
Donard97 <donardgolaj@gmail.com>
2022-01-26 20:50:07 +0100

    Add methods to meetings controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,37 @@
 class MeetingsController < ApplicationController
+  def index
+    meetings = Meeting.all
+    render json: meetings, adapter: :json_api, status: 200
+  end
+
+  def show
+    meeting = Meeting.find(params[:id])
+    render json: meeting, adapter: :json_api, status: 200
+  end
+
+  def create
+    meeting = Meeting.create(user_id: params[:user_id], meeting_type_id: params[:meeting_type_id], title: params[:title], description: params[:description],
+                            color: params[:color], image: params[:image], link: params[:link], attendee_limit: params[:attendee_limit])
+    render json: meeting, adapter: :json_api, status: 200 if meeting.save!
+  end
+
+  def update
+    meeting = Meeting.find(params[:id])
+
+    respond_to do |format|
+      if meeting.update(user_id: params[:user_id], meeting_type_id: params[:meeting_type_id], title: params[:title], description: params[:description],
+        color: params[:color], image: params[:image], link: params[:link], attendee_limit: params[:attendee_limit])
+
+        format.json {  render json: meeting, status: :ok }
+      else
+        format.json { render json: meeting.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    meeting = Meeting.find(params[:id])
+    meeting.destroy
+    render json: {message: "Meeting has been deleted succesfully!", meeting: meeting.title}, adapter: :json_api, status: 200 if meeting.destroy!
+  end
 end

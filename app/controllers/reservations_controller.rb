c7e67786bd285c4f81dd69a6e475d4bd4f985a4b
Donard97 <donardgolaj@gmail.com>
2022-01-26 21:27:07 +0100

    Add methods for reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,17 @@
 class ReservationsController < ApplicationController
+  def index
+    reservations = User.find(params[:user_id]).reservations
+    render json: reservations, adapter: :json_api, status: 200
+  end
+
+  def create
+    reservation = Reservation.create(user_id: params[:user_id], meeting_id: params[:meeting_id])
+    render json: reservation, adapter: :json_api, status: 200 if reservation.save!
+  end
+
+  def destroy
+    reservation = Reservation.find(params[:id])
+    reservation.destroy
+    render json: {message: "Reservation has been deleted succesfully!", reservation: reservation.meeting.title}, adapter: :json_api, status: 200 if reservation.destroy!
+  end
 end

6df37829cbfffd6be776d4d4c575e5561203b101
ahzia <ah.ziayosfi@gmail.com>
2022-01-27 15:09:55 +0430

    update User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_one :user_detail
   has_one :meeting
   has_many :reservations
-  validates :user_name, presence: true
+  validates :user_name, presence: true, uniqueness: true, length: { in: 4..20 }
 end

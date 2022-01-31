c36fc74d846f4cb88b316ce3c47c03a82c885c8f
ahzia <ah.ziayosfi@gmail.com>
2022-01-31 12:46:41 +0430

    implement custom json error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+
+  # CUSTOM EXCEPTION HANDLING
+  rescue_from Exception do |e|
+    error_info = format_error("internal-server-error",e);
+    render :json => error_info.to_json, :status => 500
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    error_info = format_error("RecordInvalid",e);
+    render :json => error_info.to_json, :status => 406
+  end
+
+  def format_error(error, e)
+    error_info = {
+      :error => error,
+      :message => "#{e.message}",
+      :class => "#{e.class.name}",
+      :record => e.record.errors,
+    }
+    error_info
+  end
+
 end

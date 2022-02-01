625122d5316939b025bfd8908b5c4bb2f66d3ad9
Donard97 <donardgolaj@gmail.com>
2022-02-01 19:21:14 +0100

    Fix error handlers for reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,18 @@
 class ReservationsController < ApplicationController
   def index
     reservations = User.find(params[:user_id]).reservations
-    render json: reservations, adapter: :json_api, status: 200
+    render json: reservations, adapter: :json_api, status: :ok
   end
 
   def create
     reservation = Reservation.create(user_id: params[:user_id], meeting_id: params[:meeting_id])
-    render json: reservation, adapter: :json_api, status: 200 if reservation.save!
+    render json: reservation, adapter: :json_api, status: :ok if reservation.save!
   end
 
   def destroy
     reservation = Reservation.find(params[:id])
     reservation.destroy
     render json: { message: 'Reservation has been deleted succesfully!', reservation: reservation.meeting.title },
-           adapter: :json_api, status: 200
+           adapter: :json_api, status: :ok
   end
 end

d122b47fe23b081809ca4d94efdcee5e9bedac9f
Donard97 <donardgolaj@gmail.com>
2022-02-04 12:43:48 +0100

    Update user api documentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
+  api :GET, "/users", "This is the index view for all users"
   def index
     users = User.all
     render json: users, adapter: :json_api, status: 200
   end
 
+  api :GET, "/users", "Gets the show page for a user"
   def show
     user = User.find(params[:id])
     render json: user, adapter: :json_api, status: 200
   end
 
+  api :POST, "/users", "Creates a user"
   def create
     user = User.create(user_name: params[:user_name])
     render json: user, adapter: :json_api, status: 200 if user.save!
   end
 
+  api :PUT, "/users/:id", " Update a user"
   def update
     user = User.find(params[:id])
 
@@ -26,6 +30,7 @@ class UsersController < ApplicationController
     end
   end
 
+  api :DELETE, "/users/:id", "Delete a user"
   def destroy
     user = User.find(params[:id])
     user.destroy
